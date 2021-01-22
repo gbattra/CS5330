d67ae22b27af5b728ef4cf7fb7798702e2410592
Greg Attra <greg.attra@gmail.com>
2021-01-22 16:47:07 -0500

    p1 first commit

---
+++ P1/imgDisplay.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <opencv4/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("usage: ImgDisplay <filepath>\n");
+        return -1;
+    }
+
+    
+}
