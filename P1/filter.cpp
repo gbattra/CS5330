2fa88beafd6395722ae3526148655b995b8a8030
Greg Attra <greg.attra@gmail.com>
2021-02-01 21:20:32 -0500

    made some loops more efficient:

--- P1/filter.cpp
+++ P1/filter.cpp
@@ -11,11 +11,12 @@ void convertToUchar(cv::Mat *src, cv::Mat *dst)
 {
     for (int r = 0; r < src->rows; r++)
     {
+        uchar *row = dst->ptr<uchar>(r);
         for (int c = 0; c < src->cols; c++)
         {
-            dst->ptr<uchar>(r)[c * 3 + 0] = (uchar) abs(src->ptr<short>(r)[c * 3 + 0]);
-            dst->ptr<uchar>(r)[c * 3 + 1] = (uchar) abs(src->ptr<short>(r)[c * 3 + 1]);
-            dst->ptr<uchar>(r)[c * 3 + 2] = (uchar) abs(src->ptr<short>(r)[c * 3 + 2]);
+            row[c * 3 + 0] = (uchar) abs(src->ptr<short>(r)[c * 3 + 0]);
+            row[c * 3 + 1] = (uchar) abs(src->ptr<short>(r)[c * 3 + 1]);
+            row[c * 3 + 2] = (uchar) abs(src->ptr<short>(r)[c * 3 + 2]);
         }
     }
 }
@@ -33,6 +34,9 @@ int blur5x5(cv::Mat &src, cv::Mat &dst)
 
     for (int r = 0; r < src.rows; r++)
     {
+        uchar *srow = src.ptr<uchar>(r);
+        uchar *trow = tmp.ptr<uchar>(r);
+
         for (int c = 0; c < src.cols; c++)
         {
             uchar htmp[3] = {0, 0, 0};
@@ -47,19 +51,22 @@ int blur5x5(cv::Mat &src, cv::Mat &dst)
                     col = c;
                 }
 
-                htmp[0] += src.ptr<uchar>(r)[col * 3 + 0] * filter[k] / 10;
-                htmp[1] += src.ptr<uchar>(r)[col * 3 + 1] * filter[k] / 10;
-                htmp[2] += src.ptr<uchar>(r)[col * 3 + 2] * filter[k] / 10;
+                htmp[0] += srow[col * 3 + 0] * filter[k] / 10;
+                htmp[1] += srow[col * 3 + 1] * filter[k] / 10;
+                htmp[2] += srow[col * 3 + 2] * filter[k] / 10;
             }
 
-            tmp.ptr<uchar>(r)[c * 3 + 0] = htmp[0];
-            tmp.ptr<uchar>(r)[c * 3 + 1] = htmp[1];
-            tmp.ptr<uchar>(r)[c * 3 + 2] = htmp[2];
+            trow[c * 3 + 0] = htmp[0];
+            trow[c * 3 + 1] = htmp[1];
+            trow[c * 3 + 2] = htmp[2];
         }
     }
 
     for (int r = 0; r < src.rows; r++)
     {
+        uchar *trow = tmp.ptr<uchar>(r);
+        uchar *drow = dst.ptr<uchar>(r);
+
         for (int c = 0; c < src.cols; c++)
         {
             uchar htmp[3] = {0, 0, 0};
@@ -79,9 +86,9 @@ int blur5x5(cv::Mat &src, cv::Mat &dst)
                 vtmp[2] += tmp.ptr<uchar>(row)[c * 3 + 2] * filter[k] / 10;
             }
 
-            dst.ptr<uchar>(r)[c * 3 + 0] = vtmp[0];
-            dst.ptr<uchar>(r)[c * 3 + 1] = vtmp[1];
-            dst.ptr<uchar>(r)[c * 3 + 2] = vtmp[2];
+            drow[c * 3 + 0] = vtmp[0];
+            drow[c * 3 + 1] = vtmp[1];
+            drow[c * 3 + 2] = vtmp[2];
         }
     }
 
@@ -93,6 +100,8 @@ int applySobel(cv::Mat &src, cv::Mat &dst, int *horiz_filter, int *vert_filter, 
     cv::Mat tmp = cv::Mat(dst.rows, dst.cols, dst.type(), 0.0);
     for (int r = 0; r < src.rows; r++)
     {
+        uchar *srow = src.ptr<uchar>(r);
+        short *trow = tmp.ptr<short>(r);
         for (int c = 0; c < src.cols; c++)
         {
             short htmp[3] = {0, 0, 0};
@@ -107,14 +116,14 @@ int applySobel(cv::Mat &src, cv::Mat &dst, int *horiz_filter, int *vert_filter, 
                     continue;
                 }
 
-                htmp[0] += ((short) src.ptr<uchar>(r)[col * 3 + 0]) * horiz_filter[k];
-                htmp[1] += ((short) src.ptr<uchar>(r)[col * 3 + 1]) * horiz_filter[k];
-                htmp[2] += ((short) src.ptr<uchar>(r)[col * 3 + 2]) * horiz_filter[k];
+                htmp[0] += ((short) srow[col * 3 + 0]) * horiz_filter[k];
+                htmp[1] += ((short) srow[col * 3 + 1]) * horiz_filter[k];
+                htmp[2] += ((short) srow[col * 3 + 2]) * horiz_filter[k];
             }
 
-            tmp.ptr<short>(r)[c * 3 + 0] = htmp[0];
-            tmp.ptr<short>(r)[c * 3 + 1] = htmp[1];
-            tmp.ptr<short>(r)[c * 3 + 2] = htmp[2];
+            trow[c * 3 + 0] = htmp[0];
+            trow[c * 3 + 1] = htmp[1];
+            trow[c * 3 + 2] = htmp[2];
         }
     }
 
@@ -178,13 +187,16 @@ int magnitude(cv::Mat &sx, cv::Mat &sy, cv::Mat &dst)
 {
     for (int r = 0; r < sx.rows; r++)
     {
+        short *sxrow = sx.ptr<short>(r);
+        short *syrow = sy.ptr<short>(r);
+        uchar *drow = dst.ptr<uchar>(r);
         for (int c = 0; c < sx.cols; c++)
         {
-            short *pixel_sx = &sx.ptr<short>(r)[c * 3];
-            short *pixel_sy = &sy.ptr<short>(r)[c * 3];
-            dst.ptr<uchar>(r)[3 * c + 0] = sqrt(pixel_sx[0] * pixel_sx[0] + pixel_sy[0] * pixel_sy[0]);
-            dst.ptr<uchar>(r)[3 * c + 1] = sqrt(pixel_sx[1] * pixel_sx[1] + pixel_sy[1] * pixel_sy[1]);
-            dst.ptr<uchar>(r)[3 * c + 2] = sqrt(pixel_sx[2] * pixel_sx[2] + pixel_sy[2] * pixel_sy[2]);
+            short *pixel_sx = &sxrow[c * 3];
+            short *pixel_sy = &syrow[c * 3];
+            drow[3 * c + 0] = sqrt(pixel_sx[0] * pixel_sx[0] + pixel_sy[0] * pixel_sy[0]);
+            drow[3 * c + 1] = sqrt(pixel_sx[1] * pixel_sx[1] + pixel_sy[1] * pixel_sy[1]);
+            drow[3 * c + 2] = sqrt(pixel_sx[2] * pixel_sx[2] + pixel_sy[2] * pixel_sy[2]);
         }
     }
 
@@ -207,11 +219,12 @@ int blurQuantize(cv::Mat &src, cv::Mat &dst, int levels)
     int b = 255/levels;
     for (int r = 0; r < src.rows; r++)
     {
+        uchar *row = dst.ptr<uchar>(r);
         for (int c = 0; c < src.cols; c++)
         {
-            dst.ptr<uchar>(r)[c * 3 + 0] = (dst.ptr<uchar>(r)[c * 3 + 0] / b) * b;
-            dst.ptr<uchar>(r)[c * 3 + 1] = (dst.ptr<uchar>(r)[c * 3 + 1] / b) * b;
-            dst.ptr<uchar>(r)[c * 3 + 2] = (dst.ptr<uchar>(r)[c * 3 + 2] / b) * b;
+            row[c * 3 + 0] = (row[c * 3 + 0] / b) * b;
+            row[c * 3 + 1] = (row[c * 3 + 1] / b) * b;
+            row[c * 3 + 2] = (row[c * 3 + 2] / b) * b;
         }
     }
 
@@ -222,11 +235,13 @@ int negative(cv::Mat &src, cv::Mat &dst)
 {
     for (int r = 0; r < src.rows; r++)
     {
+        uchar *drow = dst.ptr<uchar>(r);
+        uchar *srow = src.ptr<uchar>(r);
         for (int c = 0; c < src.cols; c++)
         {
-            dst.ptr<uchar>(r)[c * 3 + 0] = 255 - src.ptr<uchar>(r)[c * 3 + 0];
-            dst.ptr<uchar>(r)[c * 3 + 1] = 255 - src.ptr<uchar>(r)[c * 3 + 1];
-            dst.ptr<uchar>(r)[c * 3 + 2] = 255 - src.ptr<uchar>(r)[c * 3 + 2];
+            drow[c * 3 + 0] = 255 - srow[c * 3 + 0];
+            drow[c * 3 + 1] = 255 - srow[c * 3 + 1];
+            drow[c * 3 + 2] = 255 - srow[c * 3 + 2];
         }
     }
 
@@ -271,15 +286,20 @@ void orientation(cv::Mat *src, cv::Mat *dst)
     sobel(src, &sx, 'x');
     sobel(src, &sy, 'y');
     // cv::Canny(sx, sy, *dst, 0, 15);
+
     for (int r = 0; r < sx.rows; r++)
     {
+        short *sxrow = sx.ptr<short>(r);
+        short *syrow = sy.ptr<short>(r);
+        uchar *drow = dst->ptr<uchar>(r);
         for (int c = 0; c < sx.cols; c++)
         {
-            short *pixel_sx = &sx.ptr<short>(r)[c * 3];
-            short *pixel_sy = &sy.ptr<short>(r)[c * 3];
-            dst->ptr<short>(r)[3 * c + 0] = atan2(-pixel_sy[0], pixel_sx[0]) * 180 / 3.14159365;
-            dst->ptr<short>(r)[3 * c + 1] = atan2(-pixel_sy[1], pixel_sx[1]) * 180 / 3.14159365;
-            dst->ptr<short>(r)[3 * c + 2] = atan2(-pixel_sy[2], pixel_sx[2]) * 180 / 3.14159365;
+            short *pixel_sx = &sxrow[c * 3];
+            short *pixel_sy = &syrow[c * 3];
+            drow[3 * c + 0] = atan2(-pixel_sy[0], pixel_sx[0]) * 180 / 3.14159365;
+            drow[3 * c + 1] = atan2(-pixel_sy[1], pixel_sx[1]) * 180 / 3.14159365;
+            drow[3 * c + 2] = atan2(-pixel_sy[2], pixel_sx[2]) * 180 / 3.14159365;
         }
     }
+
 }
