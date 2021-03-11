9d21b9b66a2911b06716b0b77c6d8b3bc8855b59
Greg Attra <greg.attra@gmail.com>
2021-03-11 16:32:08 -0500

    prints out axis

--- P4/views/poseView.cpp
+++ P4/views/poseView.cpp
@@ -8,6 +8,11 @@
 #include "views/views.h"
 #include "models/models.h"
 
+std::string rotation_translation_str(
+    "R: [ %f %f %f ]\n"
+    "T: [ %f %f %f ]\n"
+    "\n"
+);
 /**
  * Render the view.
  * 
@@ -19,6 +24,15 @@ bool vw::PoseView::render(mdl::Pose model)
 {
     if (model.found)
     {
+        printf(
+            rotation_translation_str.c_str(),
+            model.rotation.at<double>(0, 0),
+            model.rotation.at<double>(0, 1),
+            model.rotation.at<double>(0, 2),
+            model.translation.at<double>(0, 0),
+            model.translation.at<double>(0, 1),
+            model.translation.at<double>(0, 2));
+        
         std::vector<cv::Point2f> img_points;
         model.points.push_back(cv::Vec3f(0, 0, 2));
 
