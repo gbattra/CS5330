5dc966aebc96c05886271eec7bee743a8afcba87
Gregory Attra <greg.attra@gmail.com>
2021-03-27 21:12:08 -0400

    trained / saved model

---
+++ P5/train.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+# Greg Attra
+# 03/27/2021
+
+"""
+This executable builds and trains a CNN to classify digits from the MNIST dataset.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# a random seed to make results reproducible
+seed = np.random.seed(42)
+
+
+def model(input_shape, n_classes):
+    """
+    Builds the model to train.
+    :param input_shape: the shape of the input images (28, 28, 1)
+    :param n_classes: the number of classes in the dataset (10)
+    :return: a Keras sequential CNN for classifying the MNIST dataset
+    """
+    m = keras.Sequential([
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Dropout(0.5),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dropout(0.5),
+        layers.Dense(n_classes, activation="softmax")
+    ])
+
+    m.summary()
+    return m
+
+
+def build_and_train(x_train, y_train, batch_size, epochs):
+    """
+    Builds and fits the model to the training data.
+    :param x_train: the training input data
+    :param y_train: the training labels
+    :param batch_size: size of each batch
+    :param epochs: number of epochs
+    :return: the compiled and trained model
+    """
+    m = model((28, 28, 1), 10)
+    m.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    m.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+
+    return m
+
+
+def evaluate(m, x_test, y_test):
+    """
+    Evaluates the model
+    :param m: the model to evaluate
+    :param x_test: the test data to evaluate with
+    :param y_test: the test labels to evaluate with
+    :return: the model score
+    """
+    score = m.evaluate(x_test, y_test)
+    return score
+
+
+def load_data():
+    """
+    Loads the MNIST data and preps it for training
+    :param filepath the path to load the data into
+    :return: the training and test datasets
+    """
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data("mnist.npz")
+
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
+    y_test = keras.utils.to_categorical(y_test, num_classes=10)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+def main():
+    """
+    Entrypoint to the program.
+    :return: 0 if success, 1 if error
+    """
+
+    # the MNIST dataset to train on
+    (x_train, y_train), (x_test, y_test) = load_data()
+    m = build_and_train(x_train, y_train, batch_size=128, epochs=10)
+    score = evaluate(m, x_test, y_test)
+
+    print("Test loss: ", score[0])
+    print("Test acc: ", score[1])
+
+    m.save("models")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
